07c62ebf56fcf5a185b5f1893d92bd455d8ac1aa
Chris Coutts <chris.dcoutts@gmail.com>
2024-02-21 12:51:46 +0000

    WIP - fetch images from JSON for image carousel

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from "../../styles/globals.module.scss";
 
-const Hero = () => {
-  return <div></div>;
+const Hero = ({ image }) => {
+  return (
+    <main>
+      <div>
+        <p>Hello World</p>
+        {image.map((data) => (
+          <img key={data.id} src={`${data.src}`} alt={`Project ${data.id}`} />
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,37 @@
 import styles from "../styles/globals.module.scss";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import NavBar from "@/components/NavBar/NavBar";
+import Hero from "@/components/Hero/Hero";
+import axios from "axios";
 
 export default function Home() {
+  const [image, setImage] = useState([]);
+
+  const fetchImages = async () => {
+    try {
+      const { data } = await axios.get("/data/images.json");
+      setImage(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  if (!image) {
+    return null;
+  }
+
   return (
     <main className={styles.globals}>
       <Head>
         <title>Chris | Full Stack Dev</title>
       </Head>
       <NavBar />
+      <Hero image={image} />
     </main>
   );
 }
